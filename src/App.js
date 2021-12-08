20cefe8d28248dce2f70e856b44249bb72c78a17
Justin W Soto <soto@pdx.edu>
2021-12-08 10:31:46 -0800

    all components made

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { TeamList } from './views/teamList/TeamList'
 import { TeamDetail } from './views/teamDetail/TeamDetail'
 import { PlayerList } from './views/playerList/PlayerList'
 import { PlayerDetail } from './views/playerDetail/PlayerDetail'
+import { Home } from './views/home/Home'
 import { Nav } from './views/navbar/Nav'
 import './App.css';
 
@@ -10,9 +11,15 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Nav />
       <main className="App">
-      
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/teamlist" exact component={TeamList} />
+        <Route path="/team/:id" exact component={TeamDetail} />
+        <Route path="/playerlist" exact component={PlayerList} />
+        <Route path="/player/:id" exact component={PlayerDetail} />
+      </Switch>
       </main>
     </Router>
   );
