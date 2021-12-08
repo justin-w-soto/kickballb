843777924b7cbeaffab4f475ab8c6a308a897afc
Justin W Soto <soto@pdx.edu>
2021-12-08 10:10:45 -0800

    init app and plugins

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css';
+
+
+function App() {
+  return (
+    <main className="App">
+     <h1>Hello Kickball</h1>
+    </main>
+  );
+}
+
+export default App;
