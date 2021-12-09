774c18b68c3068883512dcb6c509b83121eec267
Justin W Soto <soto@pdx.edu>
2021-12-09 11:35:13 -0800

    player list passing test

---
+++ src/views/playerList/PlayerList.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route } from 'react-router-dom'
+import { PlayerList } from './PlayerList';
+
+it('Should render a list of players to the screen', async () => {
+    const {container} = render(
+        <MemoryRouter>
+            <Route path="/players">
+                <PlayerList />
+            </Route>
+        </MemoryRouter>
+    )
+    screen.getByText('Players')
+    expect(container).toMatchSnapshot()
+})
