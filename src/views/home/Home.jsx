baa5c7eb6a89eaaa92d9f7525ec7247916436b6e
Justin W Soto <soto@pdx.edu>
2021-12-08 22:03:26 -0800

    added animation to home page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import kickball from '../../kickball.png'
+
 
 export const Home = () => {
     return (
         <div>
+            <img className="Ball" src={kickball} alt='red ball'/>
             <h1>Kickball</h1>
         </div>
     )
