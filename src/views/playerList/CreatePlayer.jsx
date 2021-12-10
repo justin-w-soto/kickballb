c59da9ba1ddfc01e3c2a65dd70870d6fb98e3cfb
Justin W Soto <soto@pdx.edu>
2021-12-09 21:32:25 -0800

    added create player form

--- src/views/playerList/CreatePlayer.jsx
+++ src/views/playerList/CreatePlayer.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export const CreatePlayer = () => {
     return (
-        <div>
-            ADD PLAYER HERE
-        </div>
+        <>
+        <fieldset>
+            <legend>Add a Player</legend>
+            <form>
+                <label htmlFor="name">Name</label>
+                <input id="name" name="name" type="text"/>
+
+                <label htmlFor="teamId">Team ID</label>
+                <input id="teamId" teamId="teamId" type="text"/>
+
+                <label htmlFor="position">Position</label>
+                <input id="position" position="position" type="text"/>
+                
+                <input type="submit" value="Add a Team" />
+            </form>
+        </fieldset>
+        </>
     )
 }
