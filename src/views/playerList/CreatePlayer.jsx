1e8f609e38ff0fd38e8874ea34124ae133c6f087
Justin W Soto <soto@pdx.edu>
2021-12-09 21:54:00 -0800

    added state to create player and create team views

--- src/views/playerList/CreatePlayer.jsx
+++ src/views/playerList/CreatePlayer.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const CreatePlayer = () => {
+    const [name, setName] = useState('')
+    const [teamId, setTeamId] = useState('')
+    const [position, setPosition] = useState('')
+
     return (
+
         <>
         <fieldset>
             <legend>Add a Player</legend>
             <form>
                 <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text"/>
+                <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 
                 <label htmlFor="teamId">Team ID</label>
-                <input id="teamId" teamId="teamId" type="text"/>
+                <input id="teamId" teamId="teamId" type="text" value={teamId} onChange={(e) => setTeamId(e.target.value)}/>
 
                 <label htmlFor="position">Position</label>
-                <input id="position" position="position" type="text"/>
+                <input id="position" position="position" type="text" value={position} onChange={(e) => setPosition(e.target.value)}/>
                 
                 <input type="submit" value="Add a Team" />
             </form>
--- src/views/teamList/CreateTeam.jsx
+++ src/views/teamList/CreateTeam.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const CreateTeam = () => {
+    const [name, setName] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
     return (
         <>
         <fieldset>
             <legend>Add a Team</legend>
             <form>
                 <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text"/>
+                <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 
                 <label htmlFor="city">City</label>
-                <input id="city" city="city" type="text"/>
+                <input id="city" city="city" type="text" value={city} onChange={(e) => setCity(e.target.value)}/>
 
                 <label htmlFor="state">State</label>
-                <input id="state" state="state" type="text"/>
-                
+                <input id="state" state="state" type="text" value={state} onChange={(e) => setState(e.target.value)}/>
+
                 <input type="submit" value="Add a Team" />
             </form>
         </fieldset>
