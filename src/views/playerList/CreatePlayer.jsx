f2508c27ed51d7176ace09e2f40b25e0ee21cfb8
Justin W Soto <soto@pdx.edu>
2021-12-09 22:22:48 -0800

    create team and player are functioning and rendering on page

--- src/views/playerList/CreatePlayer.jsx
+++ src/views/playerList/CreatePlayer.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { createPlayer } from '../../services/players'
 
 export const CreatePlayer = () => {
     const [name, setName] = useState('')
     const [teamId, setTeamId] = useState('')
     const [position, setPosition] = useState('')
+    const history = useHistory()
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const res = await createPlayer({ name, teamId, position })
+        history.push(`/players/${res[0].id}`)
+    }
 
     return (
-
         <>
         <fieldset>
             <legend>Add a Player</legend>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Name</label>
                 <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 
--- src/views/teamList/CreateTeam.jsx
+++ src/views/teamList/CreateTeam.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { createTeam } from '../../services/teams'
 
 export const CreateTeam = () => {
     const [name, setName] = useState('')
     const [city, setCity] = useState('')
     const [state, setState] = useState('')
+    const history = useHistory()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+       const res = await createTeam({ name, city, state })
+       history.push(`/teams/${res[0].id}`) 
+    }
+
     return (
         <>
         <fieldset>
             <legend>Add a Team</legend>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Name</label>
                 <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
 
