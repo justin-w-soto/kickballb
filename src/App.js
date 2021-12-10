58f864f83aca2df47191b09ce9567238dc66723e
Justin W Soto <soto@pdx.edu>
2021-12-09 20:55:16 -0800

    routes for create & update teams and players added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { TeamList } from './views/teamList/TeamList'
 import { TeamDetail } from './views/teamDetail/TeamDetail'
 import { PlayerList } from './views/playerList/PlayerList'
 import { PlayerDetail } from './views/playerDetail/PlayerDetail'
+import { CreateTeam } from './views/teamList/CreateTeam'
+import { UpdateTeam } from './views/teamList/UpdateTeam'
+import { CreatePlayer } from './views/playerList/CreatePlayer'
+import { UpdatePlayer } from './views/playerList/UpdatePlayer'
 import { Home } from './views/home/Home'
 import { Nav } from './views/navbar/Nav'
 import './App.css';
@@ -16,6 +20,10 @@ function App() {
       <Nav className="Navbar"/>
       </header>
       <Switch>
+        <Route path="/teams/new" exact component={CreateTeam} />
+        <Route path="/teams/update" exact component={UpdateTeam} />
+        <Route path="/players/new" exact component={CreatePlayer} />
+        <Route path="/players/update" exact component={UpdatePlayer}/>
         <Route path="/" exact component={Home} />
         <Route path="/teamlist" exact component={TeamList} />
         <Route path="/teams/:id" exact component={TeamDetail} />
